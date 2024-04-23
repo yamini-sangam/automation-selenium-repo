f3ddf5060cadf091ad2f4af505082ad01e8c6f91
yamini-sangam <85620364+yamini-sangam@users.noreply.github.com>
2024-04-23 22:54:25 +0530

    added code changes

--- DemoAutomation1/src/test/java/demoautomation1/Salesforce.java
+++ DemoAutomation1/src/test/java/demoautomation1/Salesforce.java
@@ -1,5 +1,4 @@
 package demoautomation1;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -8,37 +7,46 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+
+
 public class Salesforce {
 
-    public static void main(String[] args) throws Exception {
-    	WebDriverManager.edgedriver().setup();
+public static void main(String[] args) throws Exception {
+WebDriverManager.edgedriver().setup();
 
-        EdgeOptions options = new EdgeOptions();
+EdgeOptions options = new EdgeOptions();
 
-        // Disable notifications
-        options.addArguments("--disable-notifications");
+// Disable notifications
+options.addArguments("--disable-notifications");
 
-        WebDriver driver = new EdgeDriver(options);
-        driver.get("https://login.salesforce.com/?locale=in");
-        driver.manage().window().maximize();
-        driver.findElement(By.xpath("//input[@id='username']")).sendKeys("sdunna@miraclesoft.com");
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Ramyadunna@7");
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("//input[@id='Login']")).click();
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("//div[@class='slds-r5']")).click();
-        Thread.sleep(1000);
-        WebElement searchInput = driver.findElement(By.xpath("//label[contains(text(),'Search apps and items...')]/following::div/input[@aria-autocomplete=\"list\"]"));
-        searchInput.sendKeys("Leads");
-        Thread.sleep(1000);
-        searchInput.sendKeys(Keys.ENTER); // Press Enter after sending the text
-        Thread.sleep(2000); // Add some wait time for the search results to load
-        driver.findElement(By.xpath("//span[contains(text(),'Recently Viewed')]/preceding::h1")).click();
-        Thread.sleep(1000);
-        WebElement closeButton = driver.findElement(By.cssSelector("button[data-action='close']"));
-        closeButton.click();
-        driver.quit(); 
-    }
+WebDriver driver = new EdgeDriver(options);
+driver.get("https://login.salesforce.com/?locale=in");
+driver.manage().window().maximize();
+driver.findElement(By.xpath("//input[@id='username']")).sendKeys("sdunna@miraclesoft.com");
+Thread.sleep(1000);
+driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Ramyadunna@7");
+Thread.sleep(1000);
+driver.findElement(By.xpath("//input[@id='Login']")).click();
+Thread.sleep(4000);
+driver.findElement(By.xpath("//div[@data-aura-rendered-by=\"440:83;a\"]")).click();
+Thread.sleep(4000);
+WebElement searchInput = driver.findElement(By.xpath("//input[@placeholder=\"Search apps and items...\"]"));
+searchInput.sendKeys("Leads");
+Thread.sleep(1000);
+searchInput.sendKeys(Keys.ENTER); // Press Enter after sending the text
+Thread.sleep(3000);
+WebElement dropdownButton = driver.findElement(By.cssSelector("button[title='Select a List View: Leads']"));
+dropdownButton.click();
+Thread.sleep(2000); // Wait for 2 seconds
 
+// Click on the "All Open Leads" option directly
+driver.findElement(By.xpath("//span[text()='All Open Leads']")).click();
+Thread.sleep(2000);
+WebElement actionLink = driver.findElement(By.cssSelector("a[title='Show 3 more actions']"));
+actionLink.click();
+Thread.sleep(2000);
+WebElement printableViewLink = driver.findElement(By.cssSelector("a[title='Printable View']"));
+printableViewLink.click();
+
+}
 }
